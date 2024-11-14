7b27d09ad18d03fdd90622fa8b53b9f0925bb47c
corinnedaigle <156022147+corinnedaigle@users.noreply.github.com>
2024-11-14 10:50:32 -0500

    Add files via upload

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -5,44 +5,36 @@ using UnityEngine;
 public class MovingObject : MonoBehaviour
 {
 
-    public int whatAmI;
-    private GameManager gameManager;
+    public int myType;
 
     // Start is called before the first frame update
     void Start()
     {
-        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        
     }
 
     // Update is called once per frame
     void Update()
-    {
-        if (whatAmI == 1) 
+    {   
+        if(myType == 1)
         {
-            //I am the Bullet
-            transform.Translate(new Vector3(0, 1, 0) * Time.deltaTime * 8f);
-        } else if (whatAmI == 2)
+            //I am a bullet
+            transform.Translate(new Vector3(0, 1, 0) * Time.deltaTime * 10f);
+        } else if (myType == 2) {
+            //I am an enemy
+            transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime * 3f);
+        } else if (myType == 3)
         {
-            //I am the EnemyOne
-            transform.Translate(new Vector3(0, 1, 0) * Time.deltaTime * 4f);
-        } else if (whatAmI == 3)
-        {
-            //I am the Cloud
-            transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime * Random.Range(3f, 6f) * gameManager.cloudSpeed);
-        } else if (whatAmI == 4)
-        {
-            //I am the Powerup
-            transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime * 6f);
+            //I am a cloud
+            transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime * Random.Range(2f, 6f));
         }
 
-        if ((transform.position.y > 9f || transform.position.y <= -9f) && whatAmI != 3)
+        if ((transform.position.y > 9f ||  transform.position.y <= -9f) && myType != 3)
         {
             Destroy(this.gameObject);
-        }
-
-        if (transform.position.y <= -9f && whatAmI == 3)
+        } else if (transform.position.y <= -9f && myType == 3)
         {
-            transform.position = new Vector3(Random.Range(-12f, 12f), 9f, 0);
+            transform.position = new Vector3(Random.Range(-10f, 10f), 9f, 0);
         }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,33 +3,27 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
+
 public class Player : MonoBehaviour
 {
 
     private float horizontalInput;
     private float verticalInput;
-    private float horizontalScreenSize = 11.5f;
-    private float verticalScreenSize = 7.5f;
     private float speed;
-    private int lives;
-    private int shooting;
-    private bool hasShield;
+    private float horizontalScreenLimit;
+    private float verticalScreenLimit;
 
-    public GameManager gameManager;
-
-    public GameObject bullet;
     public GameObject explosion;
-    public GameObject thruster;
-    public GameObject shield;
+    public GameObject bullet;
+    public int lives;
 
     // Start is called before the first frame update
     void Start()
     {
         speed = 6f;
+        horizontalScreenLimit = 11.5f;
+        verticalScreenLimit = 7.5f;
         lives = 3;
-        shooting = 1;
-        hasShield = false;
-        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -43,12 +37,12 @@ public class Player : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
-        transform.Translate(new Vector3(horizontalInput, verticalInput,0) * Time.deltaTime * speed);
-        if (transform.position.x > horizontalScreenSize || transform.position.x <= -horizontalScreenSize)
+        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * Time.deltaTime * speed);
+        if (transform.position.x > horizontalScreenLimit || transform.position.x <= -horizontalScreenLimit)
         {
             transform.position = new Vector3(transform.position.x * -1, transform.position.y, 0);
         }
-        if (transform.position.y > verticalScreenSize || transform.position.y < -verticalScreenSize)
+        if (transform.position.y > verticalScreenLimit || transform.position.y <= -verticalScreenLimit)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y * -1, 0);
         }
@@ -56,106 +50,22 @@ public class Player : MonoBehaviour
 
     void Shooting()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space))
         {
-            switch (shooting)
-            {
-                case 1:
-                    Instantiate(bullet, transform.position + new Vector3(0, 1, 0), Quaternion.identity);
-                    break;
-                case 2:
-                    Instantiate(bullet, transform.position + new Vector3(-0.5f, 1, 0), Quaternion.identity);
-                    Instantiate(bullet, transform.position + new Vector3(0.5f, 1, 0), Quaternion.identity);
-                    break;
-                case 3:
-                    Instantiate(bullet, transform.position + new Vector3(-0.5f, 1, 0), Quaternion.Euler(0, 0, 30f)); 
-                    Instantiate(bullet, transform.position + new Vector3(0, 1, 0), Quaternion.identity);
-                    Instantiate(bullet, transform.position + new Vector3(0.5f, 1, 0), Quaternion.Euler(0, 0, -30f));
-                    break;
-            }
+            Instantiate(bullet, transform.position + new Vector3(0, 1, 0), Quaternion.identity);
         }
     }
 
     public void LoseALife()
     {
-        if (hasShield == false)
-        {
-            lives--;
-        } else if (hasShield == true)
-        {
-            //lose the shield
-            shield.gameObject.SetActive(false);
-            //no longer have a shield
-            hasShield = false;
-        }
-
+        //lives = lives - 1;
+        //lives -= 1;
+        lives--;
         if (lives == 0)
         {
-            gameManager.GameOver();
             Instantiate(explosion, transform.position, Quaternion.identity);
             Destroy(this.gameObject);
         }
-    }
-
-    IEnumerator SpeedPowerDown()
-    {
-        yield return new WaitForSeconds(3f);
-        speed = 6f;
-        thruster.gameObject.SetActive(false);
-        gameManager.UpdatePowerupText("");
-    }
-
-    IEnumerator ShootingPowerDown()
-    {
-        yield return new WaitForSeconds(3f);
-        shooting = 1;
-        gameManager.UpdatePowerupText("");
-    }
-
-      IEnumerator ShieldPowerDown()
-    {
-        yield return new WaitForSeconds(3f);
-        hasShield = false;
-        shield.gameObject.SetActive(false);
-        gameManager.UpdatePowerupText("");
-    }
-
-    private void OnTriggerEnter2D(Collider2D whatIHit)
-    {
-        if(whatIHit.tag == "Powerup")
-        {
-            gameManager.PlayPowerUp();
-            int powerupType = Random.Range(1, 5); //this can be 1, 2, 3, or 4
-            switch(powerupType)
-            {
-                case 1:
-                    //speed powerup
-                    speed = 9f;
-                    gameManager.UpdatePowerupText("Picked up Speed!");
-                    thruster.gameObject.SetActive(true);
-                    StartCoroutine(SpeedPowerDown());
-                    break;
-                case 2:
-                    //double shot
-                    shooting = 2;
-                    gameManager.UpdatePowerupText("Picked up Double Shot!");
-                    StartCoroutine (ShootingPowerDown());
-                    break;
-                case 3:
-                    //triple shot
-                    shooting = 3;
-                    gameManager.UpdatePowerupText("Picked up Triple Shot!");
-                    
-                    break;
-                case 4:
-                    //shield
-                    hasShield = true;
-                    gameManager.UpdatePowerupText("Picked up Shield!");
-                    shield.gameObject.SetActive(true);
-                    StartCoroutine(ShieldPowerDown());
-                    break;
-            }
-            Destroy(whatIHit.gameObject);
-        }
+        GameObject.Find("GameManager").GetComponent<GameManager>().LivesCounter(lives);
     }
 }
