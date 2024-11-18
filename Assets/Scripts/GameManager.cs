dfaad36042026f3143482617327d70daf16d4cc8
corinnedaigle <156022147+corinnedaigle@users.noreply.github.com>
2024-11-17 21:46:13 -0500

    Add files via upload

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,39 +2,71 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class GameManager : MonoBehaviour
 {
 
     public GameObject player;
-    public GameObject enemy;
+    public GameObject enemyOne;
+    public GameObject enemyTwo;
     public GameObject cloud;
-    public GameObject coin;
-    private int score;
+    public GameObject powerup;
+
+    public AudioClip powerUp;
+    public AudioClip powerDown;
+
+    public int cloudSpeed;
+
+    private bool isPlayerAlive;
 
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI livesText;
+    public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI restartText;
+    public TextMeshProUGUI powerupText;
+
+    private int score;
+    private int lives;
 
     // Start is called before the first frame update
     void Start()
     {
         Instantiate(player, transform.position, Quaternion.identity);
-        InvokeRepeating("CreateEnemy", 1f, 3f);
+        InvokeRepeating("CreateEnemyOne", 1f, 3f);
+        InvokeRepeating("CreateEnemyTwo", 2f, 2f);
+        StartCoroutine(CreatePowerup());
         CreateSky();
         score = 0;
+        lives = player.GetComponent<Player>().lives;
         scoreText.text = "Score: " + score;
-        CoinAppear();
+        livesText.text = "Lives: " + lives;
+        isPlayerAlive = true;
+        cloudSpeed = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Restart();   
     }
 
-    void CreateEnemy()
+    void CreateEnemyOne()
     {
-        Instantiate(enemy, new Vector3(Random.Range(-9f, 9f), 7.5f, 0), Quaternion.identity);
+        Instantiate(enemyOne, new Vector3(Random.Range(-9f, 9f), 7.5f, 0), Quaternion.Euler(0, 0, 180));
+    }
+
+         void CreateEnemyTwo()
+    {
+        Instantiate(enemyTwo, new Vector3(12f, Random.Range(-6f, 6f), 0), Quaternion.Euler(0, 0, 90));
+    }
+
+    IEnumerator CreatePowerup()
+    {
+        Instantiate(powerup, new Vector3(Random.Range(-9f, 9f), 7.5f, 0), Quaternion.identity);
+        yield return new WaitForSeconds(Random.Range(3f, 6f));
+        StartCoroutine(CreatePowerup());
     }
 
     void CreateSky()
@@ -45,15 +77,46 @@ public class GameManager : MonoBehaviour
         }
     }
 
-     private void CoinAppear()
+    public void EarnScore(int newScore)
     {
-        Instantiate(coin, new Vector3(Random.Range(-8f, 8f), 4f, 0), Quaternion.identity);
-    }
-
-    public void EarnScore(int howMuch) 
-    {  
-        score = score + howMuch;
+        score = score + newScore;
         scoreText.text = "Score: " + score;
     }
 
+      public void LivesCounter(int newLife) 
+    {  
+        livesText.text = "Lives: " + newLife;
+    }
+
+    public void GameOver()
+    {
+        isPlayerAlive = false;
+        CancelInvoke();
+        gameOverText.gameObject.SetActive(true);
+        restartText.gameObject.SetActive(true);
+        cloudSpeed = 0;
+    }
+
+    void Restart()
+    {
+        if(Input.GetKeyDown(KeyCode.R) && isPlayerAlive == false)
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+
+    public void UpdatePowerupText(string whichPowerup)
+    {
+        powerupText.text = whichPowerup;
+    }
+
+    public void PlayPowerUp()
+    {
+        AudioSource.PlayClipAtPoint(powerUp, Camera.main.transform.position);
+    }
+
+    public void PlayPowerDown()
+    {
+        AudioSource.PlayClipAtPoint(powerDown, Camera.main.transform.position);
+    }
 }
